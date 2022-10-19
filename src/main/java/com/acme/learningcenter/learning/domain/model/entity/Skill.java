16348cc3e2220c1148bfb776c55051da3b8fbe36
Angel Velasquez <pcisavel@upc.edu.pe>
2022-10-18 21:45:00 -0500

    feat(skills): added relationship with criterion entity.

--- src/main/java/com/acme/learningcenter/learning/domain/model/entity/Skill.java
+++ src/main/java/com/acme/learningcenter/learning/domain/model/entity/Skill.java
@@ -1,12 +1,15 @@
 package com.acme.learningcenter.learning.domain.model.entity;
 
 import com.acme.learningcenter.shared.domain.model.AuditModel;
+import com.acme.learningcenter.shared.exception.ResourceValidationException;
 import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -24,4 +27,32 @@ public class Skill extends AuditModel {
     @NotBlank
     @Size(max = 60)
     private String name;
+
+    // Relationships
+
+    @OneToMany(cascade = CascadeType.ALL,
+    fetch = FetchType.LAZY,
+    mappedBy = "skill")
+    private Set<Criterion> criteria = new HashSet<>();
+
+    public Skill addCriterion(String criterionName) {
+
+        // Initialize if null
+
+        if (criteria == null)
+            criteria = new HashSet<>();
+
+        // Criterion Name uniqueness validation under skill scope
+        if(!criteria.isEmpty())
+            if(criteria.stream().anyMatch(criterion ->
+                    criterion.getName().equals(criterionName)))
+                throw new ResourceValidationException("Criterion",
+                        "A criterion with the same name already exists for skill.");
+
+        // Add Criterion to Skill
+        criteria.add(new Criterion()
+                .withName(criterionName)
+                .withSkill(this));
+        return this;
+    }
 }
