601855547fea066a92d2cf982bfe95db2fa6d990
Angel Velasquez <pcisavel@upc.edu.pe>
2022-10-18 21:35:10 -0500

    feat(criteria): added criterion entity.

---
+++ src/main/java/com/acme/learningcenter/learning/domain/model/entity/Criterion.java
@@ -0,0 +1,34 @@
+package com.acme.learningcenter.learning.domain.model.entity;
+
+import com.acme.learningcenter.shared.domain.model.AuditModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@With
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "criteria")
+public class Criterion extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String name;
+
+    // Relationships
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "skill_id", nullable = false)
+    @JsonIgnore
+    private Skill skill;
+
+}
