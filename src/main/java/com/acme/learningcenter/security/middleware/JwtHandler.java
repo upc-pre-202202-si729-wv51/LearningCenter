543217db8a910c7a41542ab37fdb957f3821ea69
Angel Velasquez <pcisavel@upc.edu.pe>
2022-11-08 21:54:42 -0500

    feat(security): implemented jwt handler.

---
+++ src/main/java/com/acme/learningcenter/security/middleware/JwtHandler.java
@@ -0,0 +1,75 @@
+package com.acme.learningcenter.security.middleware;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.apache.commons.lang3.time.DateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtHandler {
+    private static Logger logger = LoggerFactory.getLogger(JwtHandler.class);
+
+    @Value("${authorization.jwt.secret}")
+    private String secret;
+
+    @Value("${authorization.jwt.expiration.days}")
+    private int expirationDays;
+
+    public String generateToken(Authentication authentication) {
+
+        String subject = ((UserDetailsImpl) authentication.getPrincipal()).getUsername();
+        Date issuedAt = new Date();
+        Date expiration = DateUtils.addDays(issuedAt, expirationDays);
+
+        Key secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiration)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getUsernameFrom(String token) {
+        Key secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+        return Jwts.parserBuilder()
+                .setSigningKey(secret.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secret.getBytes())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("Invalid JSON Web Token Signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JSON Web Token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JSON Web Token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JSON Web Token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JSON Web Token claims string is empty: {}", e.getMessage());
+        }
+        return false;
+
+    }
+
+}
